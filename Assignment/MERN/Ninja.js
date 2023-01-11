c4ca645ca74ee45d8db3b973b2e0839d008d7933
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-11 06:05:47 -0800

    Modified for Inheritance

--- Assignment/MERN/Ninja.js
+++ Assignment/MERN/Ninja.js
@@ -19,8 +19,26 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10){
+        super(name, health, speed, strength);
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Loka Samastha Sukino Bhavanthu");
+    }
+}
+
+
 const ninja1 = new Ninja("User1", 90);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake()
 ninja1.showStats();
+
+const teacher = new Sensei("Guru");
+teacher.showStats();
+teacher.speakWisdom();
+teacher.showStats();
