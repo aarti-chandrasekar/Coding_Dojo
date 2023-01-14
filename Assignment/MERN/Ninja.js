5220b1133b43f36c530d0be5be170e19fc7450cf
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-13 16:47:09 -0800

    Removed sub-class and added export module.

--- Assignment/MERN/Ninja.js
+++ Assignment/MERN/Ninja.js
@@ -19,26 +19,12 @@ class Ninja {
     }
 }
 
-class Sensei extends Ninja {
-    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10){
-        super(name, health, speed, strength);
-        this.wisdom = wisdom;
-    }
-
-    speakWisdom(){
-        super.drinkSake();
-        console.log("Loka Samastha Sukino Bhavanthu");
-    }
-}
-
-
 const ninja1 = new Ninja("User1", 90);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake()
 ninja1.showStats();
 
-const teacher = new Sensei("Guru");
-teacher.showStats();
-teacher.speakWisdom();
-teacher.showStats();
+module.exports = {Ninja};
+
+// console.log(module)
