ccb3d9062637b20acc2f16b02a3acab92be16dce
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-14 19:44:55 -0800

    Changed List Message

--- Assignment/MERN/first-jsx/src/components/TodoList.jsx
+++ Assignment/MERN/first-jsx/src/components/TodoList.jsx
@@ -7,7 +7,7 @@ class ToDoList extends Component {
             <>
                 <h2>Things I need to do :</h2>
                 <ul>
-                    <li>Learn React</li>
+                    <li>Learn Angular</li>
                     <li>Climb Mt. Everest</li>
                     <li>Run a marathon</li>
                     <li>Feed the dogs</li>
