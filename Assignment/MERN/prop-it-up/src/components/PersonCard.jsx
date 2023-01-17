3c6f2eae1069d1c15b28f7b6742e54d757fdc571
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-16 22:27:58 -0800

    State Demo

--- Assignment/MERN/prop-it-up/src/components/PersonCard.jsx
+++ Assignment/MERN/prop-it-up/src/components/PersonCard.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 
 export default class PersonCard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {age : props.age};
+  }
+  
   render() {
-    const {firstName:fname, lastName:lname, age, hairColor:hairCol} = this.props;
+    const {firstName:fname, lastName:lname, hairColor:hairCol} = this.props;
+    const age = this.state.age;
     return (
       <>
         <h2>{lname}, {fname}</h2>
         <h4>Age: {age}</h4>
         <h5>Hair Color: {hairCol}</h5>
+        <button onClick={() => {this.setState({age : age + 1});}}>Birthday Button for {fname} {lname}</button>
       </>
     )
   }
