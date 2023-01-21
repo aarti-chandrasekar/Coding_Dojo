135db11256489814583ecbd8bf245d32f4a9a7bb
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-20 19:28:20 -0800

    Added another solution without nested for-loops

--- Assignment/Algorithm/FindConsecutiveSum.js
+++ Assignment/Algorithm/FindConsecutiveSum.js
@@ -21,13 +21,16 @@ const sum3 = 5;
 const expected3 = [];
 
 // Bonus:
-const nums4 = [2, 5, 3, 6, 7, 0, 0, 23, 12];
+const nums4 = [2, 5, 0, 3, 6, 7, 0, 0, 23, 12];
 const sum4 = 16;
 const expected4 = [
-  [2, 5, 3, 6],
+  [2, 5, 0, 3, 6],
+  [0, 3, 6, 7],
+  [0, 3, 6, 7, 0],
+  [0, 3, 6, 7, 0, 0],
   [3, 6, 7],
   [3, 6, 7, 0],
-  [3, 6, 7, 0, 0],
+  [3, 6, 7, 0, 0]
 ];
 
 // Bonus:
@@ -39,6 +42,10 @@ const expected5 = [
   [-3, -6, -7, -0],
   [-3, -6, -7, -0, -0],
 ];
+
+const nums6 = [10, 15, 20, 35, 0, 30, 5, 10, 15, 20, 35, 0, 30, 5, 10, 15, 20, 35, 0, 30, 5, 10, 15, 20, 35, 0, 30,
+];
+const sum6 = 35;
 
 /**
  * Finds all the sets of consecutive numbers that sum to the given target sum.
@@ -52,26 +59,94 @@ const expected5 = [
  *    only.
  */
 function findConsqSums(nums, targetSum) {
+  const start = performance.now();
 
-    let result = []
-    for (let i=0; i<nums.length; i++){
-        let tot = nums[i];
-        for (let j=i+1; j<nums.length; j++){
-            if (tot === targetSum){
-                result = [...result, nums.slice(i, j)] ;
-            }
-            tot += nums[j];
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let tot = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (tot === targetSum) {
+        result = [...result, nums.slice(i, j)];
+      }
+      tot += nums[j];
 
-        }
     }
+  }
+  console.log(`This took ${performance.now() - start} milliseconds to run`);
 
-    return result;
+
+  return result;
 }
-console.log(findConsqSums(nums1, sum1))
-console.log(findConsqSums(nums2, sum2))
-console.log(findConsqSums(nums3, sum3))
-console.log(findConsqSums(nums4, sum4))
-console.log(findConsqSums(nums5, sum5))
+// console.log(findConsqSums(nums1, sum1))
+// console.log(findConsqSums(nums2, sum2))
+// console.log(findConsqSums(nums3, sum3))
+// console.log(findConsqSums(nums4, sum4))
+// console.log(findConsqSums(nums5, sum5))
+// console.log(findConsqSums(nums6, sum6))
+
+
+/**
+ * Solving without nested for-loops. Without O(N^2) time complexity
+ * Build a map/dict with key as the sum up to the current index value 
+ * and the value an array of indexes that result in that sum
+ * For a given array  [2, 5, 0, 3, 6, 7, 0, 0, 23, 12]
+ * the map will look like this {
+  '0': [ 0 ],  --> This is added by default 
+  '2': [ 1 ],  --> Sum of values up to (1-1)th index is 2 
+  '7': [ 2, 3 ],  --> Sum of values up to (2-1)th index and (3-1)th index is 7 - 2+5, 2+5+0
+  '10': [ 4 ], --> Sum of values up to (4-1)th index is 10 - 2+5+0+3
+  '16': [ 5 ],
+  '23': [ 6, 7, 8 ],
+  '46': [ 9 ],
+  '58': [ 10 ]
+  }
+* For each key in the map, if (key + targetSum) is present in the map then
+* slice the array from value of key to value of (key + targetSum)-1, if present,  and add it to result array.
+* For instance, key is 0 and  value is 0
+* (key + targetSum) is 16 and value is 5. 
+* Since the map has the key 16, slice the array from 0 to 4 - [2,5,0,3] and add it to result array.
+* Next key is 2 and value is 1
+* (key + targetSum) is 18, but the map does not have the key 18.
+* if the value of a key has more than one element in the array, repeat the slicing for each of the element as index.
+ */
+function findConsqSums2(nums, targetSum) {
+  const start = performance.now();
+
+  let result = [];
+  const sumMap = { '0': [0] };
+
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    sumMap[sum] = [...(sumMap[sum] == undefined ? [] : sumMap[sum]), i + 1];
+  }
+  console.log(sumMap);
+
+  for (key in sumMap) {
+    const startArr = sumMap[key];
+    const tsum = Number(key) + targetSum;
+    const endArr = sumMap[tsum];
+    // console.log(key + " - " + startArr + " - " + endArr + " - " + tsum);
+    if (endArr != undefined) {
+      for (let i = 0; i < startArr.length; i++) {
+        for (let j = 0; j < endArr.length; j++) {
+          result.push(nums.slice(startArr[i], endArr[j]));
+        }
+      }
+    }
+  }
+  console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+
+  return result;
+}
+// console.log(findConsqSums2(nums1, sum1))
+// console.log(findConsqSums2(nums2, sum2))
+// console.log(findConsqSums2(nums3, sum3))
+console.log(findConsqSums2(nums4, sum4))
+// console.log(findConsqSums2(nums5, sum5))
+// console.log(findConsqSums2(nums6, sum6))
+
 
 /*****************************************************************************/
 
