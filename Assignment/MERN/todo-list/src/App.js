2991fcb3595c1315d98e6ef8c4d82a41eaed4cb2
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-24 11:51:31 -0800

    Fixed sessionStorage with useEffect issue

--- Assignment/MERN/todo-list/src/App.js
+++ Assignment/MERN/todo-list/src/App.js
@@ -6,9 +6,20 @@ import { TodoItem } from './components/TodoItem';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isModified, setIsModified] = useState(0);
 
-  // useEffect(() => window.localStorage.setItem("todoList", JSON.stringify(todoList)));
-  // useEffect(() => setTodoList(JSON.parse(window.localStorage.getItem("todoList"))));
+  useEffect(() => {
+    setTodoList(JSON.parse(sessionStorage.getItem("todoList")) || [])
+    // alert("On load " + JSON.parse(sessionStorage.getItem("todoList")));
+  }, []);
+
+  useEffect(() => {
+    // alert("Update - " + isModified);
+    if (isModified !== 0) {
+      sessionStorage.setItem("todoList", JSON.stringify(todoList));
+      // alert("Not first time");
+    }
+  }, [isModified]);
 
 
   const addTodoList = (action) => {
@@ -17,16 +28,19 @@ function App() {
       action: action,
       isDone: false
     }]);
+    setIsModified(isModified + 1);
   }
 
   const markDoneTodo = (id, isDone) => {
     let newList = [...todoList];
     newList[id].isDone = isDone;
     setTodoList(newList);
+    setIsModified(isModified + 1);
   }
 
   const deleteTodo = (id, isDone) => {
     setTodoList(todoList.filter((val, index) => (index !== id)));
+    setIsModified(isModified + 1);
   }
 
 
@@ -34,7 +48,7 @@ function App() {
     <div className="App">
       <AddTodoForm addToList={addTodoList} />
 
-      {todoList.map((item, index) => 
+      {todoList.map((item, index) =>
         <TodoItem key={index} id={index} action={item.action} isDone={item.isDone} markDoneTodo={markDoneTodo} deleteTodo={deleteTodo} />
       )}
     </div>
