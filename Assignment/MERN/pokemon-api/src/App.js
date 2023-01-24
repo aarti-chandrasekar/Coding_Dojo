b844eb3d056f3b45b642a93b72d8370e42dc30f7
aarti-chandrasekar <aarti.cann@gmail.com>
2023-01-23 23:44:53 -0800

    Added axios dependency and updated API call using AXIOS

--- Assignment/MERN/pokemon-api/src/App.js
+++ Assignment/MERN/pokemon-api/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
   const [list, setList] = useState([]);
+  
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=1279";
 
   const fetchAPI = () => {
-    const url = "https://pokeapi.co/api/v2/pokemon?limit=1279";
     fetch(url)
       .then(response => {
         return response.json();
@@ -19,10 +21,20 @@ function App() {
       });
   }
 
+  const axiosAPI = () => {
+    axios.get(url)
+      .then(response => {
+        // console.log(response.data);
+        setList(response.data.results.map((item) => item.name))
+      }).catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className="row d-flex justify-content-center">
       <div className="flex-column bg-dark d-flex justify-content-center  w-50" >
-        <input type="button" value="Fetch Pokemon" onClick={fetchAPI} />
+        <input type="button" value="Fetch Pokemon" onClick={axiosAPI} />
         <ul className="mt-3 ">
           {list.map((item, index) => 
             <li key={index}>{item}</li>
