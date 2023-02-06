6a98e99415e6565945895fbb89b2a8f6bb22a549
aarti-chandrasekar <aarti.cann@gmail.com>
2023-02-06 00:03:41 -0800

    First Java Class

---
+++ Assignment/Java/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava{
+    public static void main(String[] args) {
+        System.out.println("My name is Aarti Chandrasekar");
+        System.out.println("I am 200 years old");
+        System.out.println("My hometown is Portland, OR");
+    }
+}
