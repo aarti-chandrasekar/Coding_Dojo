bcaeb3fb663fb2f7a8e2c8e46861050a35949a76
aarti-chandrasekar <aarti.cann@gmail.com>
2023-02-09 11:14:23 -0800

    Added methods - insert with recursion, array to list, list to array

--- Assignment/Algorithm/LinkedList/SinglyLinkedList.js
+++ Assignment/Algorithm/LinkedList/SinglyLinkedList.js
@@ -13,14 +13,6 @@ class ListNode {
    */
   constructor(data) {
     this.data = data;
-    /**
-     * This property is used to link this node to whichever node is next
-     * in the list. By default, this new node is not linked to any other
-     * nodes, so the setting / updating of this property will happen sometime
-     * after this node is created.
-     *
-     * @type {ListNode|null}
-     */
     this.next = null;
   }
 }
@@ -73,8 +65,100 @@ class SinglyLinkedList {
     }
   }
 
+  insertAtFront(data) {
+    const node = new ListNode(data)
+
+    if (this.isEmpty()) {
+      this.head = node
+      this.last = node
+    } else {
+      node.next = this.head
+      this.head = node
+    }
+  }
+
+  removeFromFront() {
+    if (this.isEmpty()) {
+      console.log("List is empty. Nothing to remove.")
+      return null
+    } else {
+      const data = this.head.data
+      this.head = this.head.next
+      console.log(`Removed ${data} from the front of the list`)
+      return data
+    }
+  }
+
+  removeFromBack() {
+    if (this.isEmpty()) {
+      console.log("List is empty. Nothing to remove.")
+      return null
+    } else {
+      let curr = this.head
+      while (curr.next.next !== null) {
+        curr = curr.next
+      }
+
+      const data = curr.next.data
+      this.last = curr
+      this.last.next = null
+      console.log(`Removed ${data} from the front of the list`)
+      return data
+    }
+  }
+
+  /**
+   * 
+   * @param {Array<any>} vals 
+   * @returns {SinglyLinkedList}
+   * insert each value in an array into SLL
+  */
+  seedFromArr(vals, isFront) {
+    vals.forEach(val => isFront ? this.insertAtFront(val) : this.insertAtBack(val))
+    return this
+  }
+
+
+  toArr() {
+    let newArr = []
+    let curr = this.head
+    while (curr.next !== null) {
+      newArr.push(curr.data)
+      curr = curr.next
+    }
+    newArr.push(curr.data)
+    return newArr
+  }
+
+  /**
+* Creates a new node with the given data and inserts it at the back of
+* this list.
+* - Time: O(?).
+* - Space: O(?).
+* @param {any} data The data to be added to the new node.
+* @param {?ListNode} runner The current node during the traversal of this list
+*    or null when the end of the list has been reached.
+* @returns {SinglyLinkedList} This list.
+*/
+  insertAtBackRecursive(data, runner = this.head) {
+    
+    if (this.isEmpty()) {
+      const node = new ListNode(data)
+      this.head = node
+      this.last = node
+    } else if (runner.next === null) {
+      const node = new ListNode(data)
+      runner.next = node
+      this.last = node
+    } else {
+      this.insertAtBackRecursive(data, runner.next)
+    }
+  }
+
+
   printList() {
     let curr = this.head
+    console.log("Current List Values :")
     while (curr.next !== null) {
       console.log(curr.data)
       curr = curr.next
@@ -87,5 +171,42 @@ const sll = new SinglyLinkedList()
 sll.insertAtBack(5)
 sll.insertAtBack(10)
 sll.insertAtBack(3)
-
+console.log("Added values at back/end - 5 10 3")
 sll.printList()
+
+sll.insertAtFront(25)
+sll.insertAtFront(1)
+sll.insertAtFront(43)
+console.log("Added values at front/beginning - 25 1 43")
+sll.printList()
+
+console.log("Array --->", sll.toArr())
+
+sll.removeFromFront()
+console.log("After Removing value at front/beginning ")
+sll.printList()
+
+sll.removeFromBack()
+console.log("After Removing value at back/end  ")
+sll.printList()
+
+console.log("Create a new Linked List from the array (add to front)- [7, 4, 9, 1, 5]")
+const sll2 = new SinglyLinkedList()
+sll2.seedFromArr([7, 4, 9, 1, 5], true)
+sll2.printList()
+
+console.log("Create a new Linked List from the array (add to back)- [7, 4, 9, 1, 5]")
+const sll3 = new SinglyLinkedList()
+sll3.seedFromArr([7, 4, 9, 1, 5])
+sll3.printList()
+
+const sll4 = new SinglyLinkedList()
+sll4.insertAtBackRecursive(9)
+sll4.insertAtBackRecursive(22)
+sll4.insertAtBackRecursive(4)
+sll4.insertAtBackRecursive(27)
+sll4.insertAtBackRecursive(16)
+console.log("Added values at back/end using RECURSION method- 9 22 4 27 16")
+sll4.printList()
+
+
