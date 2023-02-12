cd11600bc116754d926a05676e406c96c4ae546c
aarti-chandrasekar <aarti.cann@gmail.com>
2023-02-11 22:12:37 -0800

    Query Params

---
+++ Assignment/Java-Spring/human/src/main/java/com/codindojo/human/controllers/HumanController.java
@@ -0,0 +1,30 @@
+package com.codindojo.human.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HumanController {
+    @RequestMapping("")
+    public String display(@RequestParam(required = false, value = "name") String name,
+                          @RequestParam(required = false, value = "last_name") String lastName,
+                          @RequestParam(required = false, value = "times") String times) {
+        System.out.println(name + " " + lastName + " " + times);
+        String retStr = " Hello ";
+        retStr += (name == null) ? "Human " : name + " ";
+        retStr += (lastName == null) ? "" : lastName + " ";
+
+        int noTimes = 1;
+        try {
+            noTimes = Integer.parseInt(times);
+        } catch (NumberFormatException e) {
+//            e.printStackTrace();
+        }
+
+        System.out.println("Output -->" + retStr);
+        return retStr.repeat(noTimes);
+
+    }
+}
