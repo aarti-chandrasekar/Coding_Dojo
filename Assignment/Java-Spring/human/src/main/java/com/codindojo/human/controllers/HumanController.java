f9a48adee4577a27d69d9dcc4e1c06bc5c63d83c
aarti-chandrasekar <aarti.cann@gmail.com>
2023-02-11 22:37:18 -0800

    Changed RequestParam to Integer from String

--- Assignment/Java-Spring/human/src/main/java/com/codindojo/human/controllers/HumanController.java
+++ Assignment/Java-Spring/human/src/main/java/com/codindojo/human/controllers/HumanController.java
@@ -10,21 +10,21 @@ public class HumanController {
     @RequestMapping("")
     public String display(@RequestParam(required = false, value = "name") String name,
                           @RequestParam(required = false, value = "last_name") String lastName,
-                          @RequestParam(required = false, value = "times") String times) {
+                          @RequestParam(required = false, value = "times") Integer times) {
         System.out.println(name + " " + lastName + " " + times);
         String retStr = " Hello ";
         retStr += (name == null) ? "Human " : name + " ";
         retStr += (lastName == null) ? "" : lastName + " ";
 
-        int noTimes = 1;
-        try {
-            noTimes = Integer.parseInt(times);
-        } catch (NumberFormatException e) {
-//            e.printStackTrace();
-        }
+//        int noTimes = 1;
+//        try {
+//            noTimes = Integer.parseInt(times);
+//        } catch (NumberFormatException e) {
+////            e.printStackTrace();
+//        }
 
         System.out.println("Output -->" + retStr);
-        return retStr.repeat(noTimes);
+        return retStr.repeat(times==null ? 1 : times);
 
     }
 }
