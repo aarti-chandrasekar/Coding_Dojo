8ae9e2cb8433642f8cc8ac34d058bbd59c55d034
aarti-chandrasekar <aarti.cann@gmail.com>
2023-02-11 22:47:32 -0800

    Path Variable

--- Assignment/Java-Spring/daikichi/src/main/java/com/codindojo/daikichi/controllers/DaikichiController.java
+++ Assignment/Java-Spring/daikichi/src/main/java/com/codindojo/daikichi/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.codindojo.daikichi.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,20 @@ public class DaikichiController {
     @RequestMapping("/tomorrow")
     public String tomorrow() {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+    @RequestMapping("/travel/{city}")
+    public String travel(@PathVariable("city") String city) {
+        return "Congratulations! You will soon travel to " + city;
+    }
+
+    @RequestMapping("/lotto/{num}")
+    public String lotto(@PathVariable("num") Integer num) {
+        return switch (num % 2) {
+            case 0 -> "You will take a grand journey in the near future, but be weary of tempting offers";
+            case 1 -> "You have enjoyed the fruits of your labor but now is a great time to spend time with" +
+                    " family and friends.";
+            default -> "Not a valid path";
+        };
     }
 }
